15ef02b6e8c6e5268e0c317a882577ca0c9a3816
kailai-13 <kailainathan2006@gmail.com>
2025-02-05 11:24:14 +0530

    finally i fixed the github bug

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI 
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
